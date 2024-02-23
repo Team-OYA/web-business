b02c1c5dff1eac39c5b8b13ca01ece8291dac9d9
yukong <kyb062666@gmail.com>
2024-02-23 19:08:21 +0900

    [FIX] bootstrap 제거

--- src/components/common/Button/Button.js
+++
@@ -1,9 +0,0 @@
-import './Button.css';
-
-function Button(props) {
-    return (
-        <button type="submit" className="btn btn-gradient-primary mr-2">{props.text}</button>
-    )
-}
-
-export default Button;
