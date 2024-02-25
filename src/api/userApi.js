b420dfdd99eae5e77577320658e1d911a50cbc55
sangminee <lsmlee99@gmail.com>
2024-02-25 23:00:57 +0900

    [CHORE] 토큰값 삭제

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const Token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJsc21sZWU5OUBnbWFpbC5jb20iLCJpYXQiOjE3MDg3ODkyMjIsImV4cCI6MTcwODg3NTYyMiwic3ViIjoiZ2h0bHN0bmY1MTY2QG5hdmVyLmNvbSIsImlkIjoxNDV9.24W-qveBdI4Eu4OQkroEzAOUnylz4n9cEFixTHleTPc"
+const Token = "{token}"
 
 if (Token) {
     axios.defaults.headers.common['Authorization'] = `Bearer ${Token}`
