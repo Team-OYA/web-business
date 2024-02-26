c47dc55b68e7b222422db33fba58f72b33be6b4f
yukong <kyb062666@gmail.com>
2024-02-26 19:15:19 +0900

    [REFACTOR] 광고 결제 페이지 division 추가

--- src/pages/business/ad.jsx
+++ src/pages/business/ad.jsx
@@ -17,43 +17,49 @@ import Button from "../../components/common/Button/Button";
 const Ad = () => {
     return (
         <>
-            <ContentBox
-                title="광고 금액 선정"
-                content={
-                    <>
-                        <RadioGroup title="광고 분류" content={["팝업스토어 게시글", "커뮤니티 게시글"]}/>
-                        <InputText title="광고 금액"/>
-                    </>
-                }/>
-            <ContentBox
-                title="게시글 선택"
-                content={<CheckedPostTable/>}/>
-            <ContentBox
-                title="주문자 정보"
-                content={
-                <>
-                    <InputText title="주문자" value="김유빈" disabled="true"/>
-                    <InputText title="연락처" value="010-1234-5678" disabled="true"/>
-                    <InputText title="이메일" value="hansalchai789@gmail.com" disabled="true"/>
-                    <InputText title="사업자 등록번호" value="109-81-72945" disabled="true"/>
-                </>
-                }/>
-            <ContentBox
-                title="결제 방법"
-                content={
-                    <>
-                        <PaymentButton text="토스페이" url={TossImage}/>
-                        <Checkbox text="주문 내용을 확인했으며, 아래 내용에 모두 동의합니다." />
-                    </>
-                }/>
-            <ContentBox
-                title="최종 결제금액"
-                content={
-                    <>
-                        <InputText title="총 결제금액" value="1,000,000원" disabled="true"/>
-                        <Button text="결제하기"/>
-                    </>
-                }/>
+            <div className="flex">
+                <div className="flex-auto">
+                    <ContentBox
+                        title="광고 금액 선정"
+                        content={
+                            <>
+                                <RadioGroup title="광고 분류" content={["팝업스토어 게시글", "커뮤니티 게시글"]}/>
+                                <InputText title="광고 금액"/>
+                            </>
+                        }/>
+                    <ContentBox
+                        title="게시글 선택"
+                        content={<CheckedPostTable/>}/>
+                </div>
+                <div className="flex-auto">
+                    <ContentBox
+                        title="주문자 정보"
+                        content={
+                            <>
+                                <InputText title="주문자" value="김유빈" disabled="true"/>
+                                <InputText title="연락처" value="010-1234-5678" disabled="true"/>
+                                <InputText title="이메일" value="hansalchai789@gmail.com" disabled="true"/>
+                                <InputText title="사업자 등록번호" value="109-81-72945" disabled="true"/>
+                            </>
+                        }/>
+                    <ContentBox
+                        title="결제 방법"
+                        content={
+                            <>
+                                <PaymentButton text="토스페이" url={TossImage}/>
+                                <Checkbox text="주문 내용을 확인했으며, 아래 내용에 모두 동의합니다."/>
+                            </>
+                        }/>
+                    <ContentBox
+                        title="최종 결제금액"
+                        content={
+                            <>
+                                <InputText title="총 결제금액" value="1,000,000원" disabled="true"/>
+                                <Button text="결제하기"/>
+                            </>
+                        }/>
+                </div>
+            </div>
         </>
     )
 }
