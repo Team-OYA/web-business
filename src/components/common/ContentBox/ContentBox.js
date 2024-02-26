82e3f66efc3ecb2501b98865e65e696262dc06d4
yukong <kyb062666@gmail.com>
2024-02-26 10:04:59 +0900

    [FEAT] 전역 Background 색상 지정

--- src/components/common/ContentBox/ContentBox.js
+++ src/components/common/ContentBox/ContentBox.js
@@ -8,7 +8,7 @@ export default ContentBox;
  */
 function ContentBox(props) {
     return (
-        <div className="shadow-md w-11/12 mx-auto my-6 rounded-none text-black-text-color-950">
+        <div className="shadow-md bg-white w-11/12 mx-auto my-6 rounded-none text-black-text-color-950">
             <h1 className="p-4 text-lg font-bold">{props.title}</h1>
             <hr className="h-px my-2 bg-gray-200 border-0" />
             <div className="p-4">
