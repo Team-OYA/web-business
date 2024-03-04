65b0ebdcbf7d65c6076b2fe767466c5c26663816
yukong <kyb062666@gmail.com>
2024-03-04 16:50:57 +0900

    [REFACTOR] 결제하기 버튼 추가

--- src/components/common/Button/PaymentButton.js
+++ src/components/common/Button/PaymentButton.js
@@ -5,15 +5,16 @@
  * @since 2024.02.26
  * @author 김유빈
  */
-function PaymentButton({text, url, onClick, buttonClassName, imgClassName}) {
+function PaymentButton({text, value, url, onClick, buttonClassName, imgClassName, selectedItem}) {
     return (
         <div>
             <button type="button"
+                    style={{backgroundColor: selectedItem === value ? "#F8F8F8" : "white"}}
                     className={`text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4
                     focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center
                     inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700
                     dark:text-white dark:hover:bg-gray-700 me-2 mb-2 h-12 w-28 ${buttonClassName}`}
-                    onClick={onClick}>
+                    onClick={() => onClick(value)}>
                 {
                     url &&
                     <div className="flex items-center justify-center">
