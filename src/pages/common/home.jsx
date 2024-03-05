9c532030fd40d6c1a669f489db3e89617f0975a6
LSMJJAng <127660101+LSMJJAng@users.noreply.github.com>
2024-03-05 15:40:04 +0900

    [RELEASE]v1.2 (#30)
    [RELEASE]v1.2

--- src/pages/common/home.jsx
+++ src/pages/common/home.jsx
@@ -1,5 +1,6 @@
 import OutlineCircleDisabledButton from "../../components/common/Button/OutlineCircleDisabledButton";
 import React from "react";
+import BgImage from "../../assets/images/img_bg.jpg";
 
 /**
  * Home 페이지 제작
@@ -9,21 +10,27 @@ import React from "react";
  */
 const Home = () => {
     return (
-        <div className="login bg-white">
-            <section>
+        <div className="login bg-cover bg-center relative" style={{ backgroundImage: `url(${BgImage})` }}>
+            <div className="overlay" style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                zIndex: 1,
+                pointerEvents: 'none', // 이 부분을 추가
+            }}></div>
+            <section style={{ position: 'relative', zIndex: 2 }}>
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                    <div
-                        className="w-full  rounded-lg md:mt-0 sm:max-w-md xl:p-0">
-                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                            <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-main-color-600 md:text-2xl">
+                    <div className="w-full rounded-lg md:mt-0 sm:max-w-md xl:p-0" style={{ position: 'relative' }}>
+                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8 z-40 mb-6">
+                            <h1 className="text-center font-semibold leading-tight tracking-tight text-white text-7xl">
                                 THE POP
                             </h1>
-                            <p className="text-center text-main-color-600">
-                                현대백화점 팝업스토어 관리 플랫폼
-                            </p>
-                            <div className="flex flex-col items-center justify-center md-8">
-                                <OutlineCircleDisabledButton text = "관리자 페이지" herf="/login/admin" info="관리자 페이지에서 유저 관리를 진행해 보세요."/>
-                                <OutlineCircleDisabledButton text = "사업체 페이지" herf="/login/business" info="사업체 페이지에서 사업계획서 및 팝업 관리를 진행해 보세요."/>
+                            <div className="flex flex-row items-center justify-center pt-10 z-50">
+                                <OutlineCircleDisabledButton text="관리자 페이지" href="/login/admin" info="관리자 페이지에서 유저 관리를 진행해 보세요."/>
+                                <OutlineCircleDisabledButton text="사업체 페이지" href="/login/business" info="사업체 페이지에서 사업계획서 및 팝업 관리를 진행해 보세요."/>
                             </div>
                         </div>
                     </div>
