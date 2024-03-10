4cb59d58d5047162217b8348dfc920d14268949a
yukong <kyb062666@gmail.com>
2024-03-10 21:52:13 +0900

    [FEAT] 사업계획서 철회 API 연동

--- src/components/Plan/PlanDetail.jsx
+++ src/components/Plan/PlanDetail.jsx
@@ -1,6 +1,7 @@
 import TwoInput from "../common/Input/TwoInput";
 import InputDate from "../common/Input/InputDate";
 import InputText from "../common/Input/InputText";
+import WithdrawPlan from "../../api/business/planDetail/withdrawPlan";
 
 /**
  * PlanDetail 컴포넌트 생성
@@ -29,6 +30,23 @@ const PlanDetail = ({planData}) => {
         document.body.removeChild(link);
     };
 
+    /**
+     * 입점 철회 api 연동
+     *
+     * @since 2024.03.10
+     * @author 김유빈
+     */
+    const handleWithdraw = async () => {
+        try {
+            const response = await WithdrawPlan.withdraw(planData.planId);
+            if (response.status === 200) {
+                window.location.href = `/plans/${planData.planId}`
+            }
+        } catch (error) {
+            console.error('Error approving plan:', error);
+        }
+    };
+
     return (
         <>
             <TwoInput
@@ -54,6 +72,27 @@ const PlanDetail = ({planData}) => {
                 >
                     파일 다운로드
                 </button>
+            </div>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <div style={{ display: 'flex', marginTop: '20px' }}>
+                    {
+                        planData.entranceStatus === '입점 요청' && (
+                            <button
+                                style={{
+                                    padding: '10px 20px',
+                                    backgroundColor: 'LightSkyBlue',
+                                    color: 'white',
+                                    borderRadius: '5px',
+                                    margin: '0 5px',
+                                    cursor: 'pointer',
+                                }}
+                                onClick={handleWithdraw}
+                                disabled={false}>
+                                입점 철회
+                            </button>
+                        )
+                    }
+                </div>
             </div>
         </>
     )
